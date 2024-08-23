09120d0c57e5ee833dd461e270719d70d2aefadf
baonghiem <nghiembaospkit@gmail.com>
2024-08-24 05:43:40 +0700

    source code frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "@/providers";
+import GlobalLoading from "./components/globalLoading";
+import { LoadingProvider } from "./context/loadingContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <LoadingProvider>
+            <GlobalLoading />
+            {children}
+          </LoadingProvider>
+        </Providers>
+      </body>
     </html>
   );
 }
